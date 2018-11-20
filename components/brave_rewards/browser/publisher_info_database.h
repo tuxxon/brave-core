77c1323acd7591fddf1d9c7a255d261fc080c84f
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-11-20 12:32:25 +0100

    Renames GetPublisherInfoList into GetActivityInfoList

--- components/brave_rewards/browser/publisher_info_database.h
+++ components/brave_rewards/browser/publisher_info_database.h
@@ -47,7 +47,7 @@ class PublisherInfoDatabase {
 
   bool InsertOrUpdateActivityInfo(const ledger::PublisherInfo& info);
 
-  bool GetPublisherActivityList(int start,
+  bool GetActivityList(int start,
                                 int limit,
                                 const ledger::ActivityInfoFilter& filter,
                                 ledger::PublisherInfoList* list);
